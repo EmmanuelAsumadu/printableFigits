2b47ac57b5522b5b0168a271c3fac84ef0934184
technocopia <technocopia@technocopia-ThinkPad-T431s>
2024-03-30 15:06:34 -0400

    working sheet

--- chainMail.groovy
+++ chainMail.groovy
@@ -7,6 +7,7 @@ double linkY = 2.66;
 double linkZ = 4.00;
 double champThickness=1.35;
 double champher =champThickness/2-0.05
+double movement = -linkX/2;
  CSG crossBar = new ChamferedCube(
 	linkX, 
 	linkY,
@@ -19,4 +20,19 @@ CSG theBottom = crossBar.union(bottomRight).union(bottomLeft).toZMin()
 CSG theTop= theBottom.rotz(90).toZMax().movez(linkZ)
 CSG post =new ChamferedCube(linkY,linkY,linkZ,champher).toCSG()
 		.toZMin()
-return[theBottom,theTop,post];
+CSG post1= post.movex(movement).movey(movement)
+CSG post2= post.movex(-movement).movey(-movement)
+CSG post3=post.movex(movement).movey(-movement)
+CSG post4=post.movex(-movement).movey(movement)
+CSG allUnion =CSG.unionAll(theBottom,theTop,post,post1,post2,post3,post4)
+def list =[]
+for (int row=0; row<12; row++) 
+{
+	for(int col=0; col<12; col++ )
+	{
+		CSG tryNew = allUnion.movex((movement*1.5)*row).movey((movement*1.5)*col)
+		
+		list.add(tryNew)
+	}
+}
+return list;
